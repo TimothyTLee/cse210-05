dfb186b39daadcce5a116addc7bb1c5ceaffadc4
TimothyTLee <timothy.lee1113@gmail.com>
2022-10-13 19:53:14 -0600

    initial commit

---
+++ cycle/game/scripting/control_actors_action.py
@@ -0,0 +1,77 @@
+import constants
+from game.scripting.action import Action
+from game.shared.point import Point
+
+
+class ControlActorsAction(Action):
+    """
+    An input action that controls the snake.
+
+    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+
+    Attributes:
+        _keyboard_service (KeyboardService): An instance of KeyboardService.
+    """
+
+    def __init__(self, keyboard_service):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
+
+    def execute(self, cast, script):
+        """Executes the control actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        _player = 0
+        # left
+        if self._keyboard_service.is_key_down('a'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+            _player = 1
+
+        # right
+        if self._keyboard_service.is_key_down('d'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+            _player = 1
+
+        # up
+        if self._keyboard_service.is_key_down('w'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+            _player = 1
+
+        # down
+        if self._keyboard_service.is_key_down('s'):
+            self._direction = Point(0, constants.CELL_SIZE)
+            _player = 1
+
+         # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+            _player = 2
+
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+            _player = 2
+
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+            _player = 2
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
+            _player = 2
+
+        for cycle in cast.get_actors("snakes"):
+            if(_player == 1 and cycle.get_color() == constants.RED):
+                cycle.turn_head(self._direction)
+            elif(_player == 2 and cycle.get_color() == constants.GREEN):
+                cycle.turn_head(self._direction)
